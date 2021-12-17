288291c176531ae5010e8a31c400583b68b550e2
Maxence Ramael <maxence.ramael@gmail.com>
2021-12-17 15:45:10 +0100

    fin pjt + btn disabled conditionnel

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,36 +3,74 @@ import Item from "./Item";
 import { v4 as uuidv4 } from "uuid";
 
 export default function Form() {
+  const [dataArr, setDataArr] = useState([]);
 
-    const [dataArr, setDataArr] = useState([
-    { txt: "premener le chien", id: uuidv4() },
-    { txt: "Faire du Sport", id: uuidv4() },
-    { txt: "Coder avec React", id: uuidv4() },
-  ]);
+  const [stateInput, setStateInput] = useState();
 
-  const deleteElement = id => {
-    const filteredState = dataArr.filter(item => {
-        return item.id !== id;
-    })
-    setDataArr(filteredState)
+  const deleteElement = (id) => {
+    const filteredState = dataArr.filter((item) => {
+      return item.id !== id;
+    });
+    setDataArr(filteredState);
   };
+
+  
+
+  const linkedInput = (e) => {
+    setStateInput(e);
+  
+
+    if (e !== "") {
+      document.getElementById("submit").removeAttribute("disabled");
+    }
+  };
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    const newArr = [...dataArr];
+
+    const newTodo = {};
+    newTodo.txt = stateInput;
+    newTodo.id = uuidv4();
+
+    newArr.push(newTodo);
+    setDataArr(newArr);
+
+    setStateInput("");
+  };
+
   return (
     <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
-      <form className="mb-3">
+      <form onSubmit={(e) => addTodo(e)} className="mb-3">
         <label htmlFor="todo" className="form-label mt-3">
           Chose à faire
         </label>
-        <input type="text" className="form-control" id="todo" />
+        <input
+          value={stateInput}
+          type="text"
+          className="form-control"
+          id="todo"
+          onInput={(e) => linkedInput(e.target.value)}
+        />
 
-        <button className="mt-2 btn btn-primary d-block">Envoyer</button>
+        <button className="mt-2 btn btn-primary d-block" id="submit" disabled>
+          Envoyer
+        </button>
       </form>
       <h2>Liste des choses à faire : </h2>
       <ul className="list-group">
-      {dataArr.map(item => {
-        return <Item txt={item.txt} id={item.id} key={item.id} delFunc={deleteElement} />;
-      })}
+        {dataArr.map((item) => {
+          return (
+            <Item
+              txt={item.txt}
+              id={item.id}
+              key={item.id}
+              delFunc={deleteElement}
+            />
+          );
+        })}
       </ul>
-     
     </div>
   );
 }
